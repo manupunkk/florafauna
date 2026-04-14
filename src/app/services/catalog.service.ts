2f78efa7671436f75fd9b0fef49ca044f5548bd3
manupunkk <151047602+manupunkk@users.noreply.github.com>
2026-04-13 21:31:00 -0400

    imagenes y orden

--- src/app/services/catalog.service.ts
+++ src/app/services/catalog.service.ts
@@ -9,31 +9,31 @@ export class CatalogService {
   private products: Product[] = [
     {
       id: 'p1',
-      name: 'Gorro Bordado Huemul',
-      description: 'Hermoso gorro de algodón con un bordado minimalista del Huemul chileno. Ideal para caminatas.',
+      name: 'Jockey Bordado Huemul',
+      description: 'Hermoso jockey de malla con un bordado minimalista del Huemul chileno. Ideal para caminatas.',
       price: 15990,
-      imageUrl: 'https://images.unsplash.com/photo-1521369909029-281b6bc93abf?auto=format&fit=crop&q=80&w=600'
+      imageUrl: '/assets/imagen/GorroHuemul.jpg'
     },
     {
       id: 'p2',
       name: 'Polera Vuelo del Cóndor',
       description: 'Polera de diseño sustentable con una ilustración estilizada del Cóndor Andino surcando los cielos.',
       price: 18500,
-      imageUrl: 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?auto=format&fit=crop&q=80&w=600'
+      imageUrl: '/assets/imagen/PoleraCondor.jpg'
     },
     {
       id: 'p3',
       name: 'Taza Zorro de Darwin',
       description: 'Taza de cerámica premium pintada a mano. Ayuda a crear conciencia sobre este enigmático cánido.',
       price: 8900,
-      imageUrl: 'https://images.unsplash.com/photo-1514228742587-6b1558fcca3d?auto=format&fit=crop&q=80&w=600'
+      imageUrl: '/assets/imagen/TazaZorro.jpg'
     },
     {
       id: 'p4',
-      name: 'Bolso Ecológico Copihue',
-      description: 'Tote bag de tela reciclada con el estampado de la flor nacional, el Copihue.',
-      price: 12000,
-      imageUrl: 'https://images.unsplash.com/photo-1597484661643-2f5fef640dd1?auto=format&fit=crop&q=80&w=600'
+      name: 'Polerón Chiporro Outdoor',
+      description: 'Polerón de chiporro ultra cálido diseñado para las mañanas frías de la Patagonia. Muy cómodo y resistente.',
+      price: 24900,
+      imageUrl: '/assets/imagen/PoleronChiporro.jpg'
     }
   ];
 
